ca257ecd943ffa5659349634501f6135875f2075
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-01 15:23:28 +0500

    добавлена админка приложения 'users'

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from users.models import CustomUser
 
-# Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    list_filter = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('id',)
