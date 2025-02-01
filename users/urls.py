68975abd43b8cfd6c888de8fe67428baaaf93ec8
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-01 15:11:51 +0500

    добавлены маршруты для просмотра и редактирования профиля

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView, email_verification
+from .views import RegisterView, email_verification, UserProfileDetailView, UserProfileUpdateView
 
 from users.apps import UsersConfig
 
@@ -10,5 +10,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
-    path('email-confirm/<str:token>/', email_verification, name='email_confirm')
+    path('email-confirm/<str:token>/', email_verification, name='email_confirm'),
+    path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/edit/<int:pk>/', UserProfileUpdateView.as_view(), name='edit_profile'),
 ]
