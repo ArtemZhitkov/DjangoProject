62c9df6b60d74d979ceb51d1b2270e6e67b6d1f2
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-04 23:37:05 +0500

    исправления

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,4 @@ from users.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
-    list_filter = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('id',)
+    exclude = ('password',)
