b8f7ff927f3c983e910dd93a13ea331225941b14
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-09 00:31:59 +0500

    добавлены сервисные функции модели Категории

---
+++ catalog/services.py
@@ -0,0 +1,16 @@
+from .models import Category, Product
+
+class CategoryService:
+    @staticmethod
+    def get_all_categories():
+        """
+        Возвращает все категории
+        """
+        return Category.objects.all()
+
+    @staticmethod
+    def get_products_from_category(category):
+        """
+        Возвращает продукты из указанной категории
+        """
+        return Product.objects.filter(category=category)
