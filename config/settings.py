662f24b9904efd2de1f4aa33410dfc0527476ef2
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-09 00:30:54 +0500

    add settings redis

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,13 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') else False
 EMAIL_USE_TLS = False if os.getenv('EMAIL_USE_TLS') else True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# Redis setting
+CACHE_ENABLED = os.getenv('CACHE_ENABLED')
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': os.getenv('LOCATION'),
+        }
+    }
